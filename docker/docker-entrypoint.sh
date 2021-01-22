19718f0564294906e304b7ff929c936f03b4ff02
Marc Hermans <marc.hermans@ldtteam.com>
2021-01-22 08:55:51 +0100

    Build a running image with support for proper config and data volumes.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,10 +1,9 @@
-﻿#!/bin/sh -x
-
-set -e
+﻿set -e
 
 # volume mounts
-config_volume=${WORKING_DIRECTORY}/wso2-config-volume
-artifact_volume=${WORKING_DIRECTORY}/wso2-artifact-volume
+config_volume=/wso2-config-volume
+artifact_volume=/wso2-artifact-volume
+database_template=/wso2-is-db-base
 
 # check if the WSO2 non-root user home exists
 test ! -d ${WORKING_DIRECTORY} && echo "WSO2 Docker non-root user home does not exist" && exit 1
@@ -17,6 +16,9 @@ test ! -d ${WSO2_SERVER_HOME} && mkdir -p ${config_volume}
 
 # copy all configuration files if they do not exist into the config volume so that they can be configured properly for the next start.
 rsync -a --prune-empty-dirs --ignore-existing --include '*/' --include '*.xml' --exclude '*' ${WSO2_SERVER_HOME}/ ${config_volume}/
+
+# copy all db files if they do not exist into the db volume so that they can be configured properly for the next start.
+rsync -a --prune-empty-dirs --ignore-existing --include '*/' ${database_template}/ ${WSO2_SERVER_HOME}/repository/database/
 
 # copy any configuration changes mounted to config_volume
 test -d ${config_volume} && [ "$(ls -A ${config_volume})" ] && cp -RL ${config_volume}/* ${WSO2_SERVER_HOME}/
