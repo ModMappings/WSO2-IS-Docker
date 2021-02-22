a373243dac641871c33e3c087e0ef9b53dd963d6
Marc Hermans <marc.hermans@ldtteam.com>
2021-02-22 08:54:00 +0100

    Updated the docker files to run the config copy directly and added *.toml files to be used in configuration.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,4 +1,6 @@
-﻿set -e
+﻿#!/usr/bin/env bash
+
+set -e
 
 # volume mounts
 config_volume=/wso2-config-volume
@@ -16,14 +18,28 @@ test ! -d ${WSO2_SERVER_HOME} && mkdir -p ${config_volume}
 
 # copy all configuration files if they do not exist into the config volume so that they can be configured properly for the next start.
 rsync -a --prune-empty-dirs --ignore-existing --include '*/' --include '*.xml' --exclude '*' ${WSO2_SERVER_HOME}/ ${config_volume}/
+rsync -a --prune-empty-dirs --ignore-existing --include '*/' --include '*.toml' --exclude '*' ${WSO2_SERVER_HOME}/ ${config_volume}/
 
 # copy all db files if they do not exist into the db volume so that they can be configured properly for the next start.
 rsync -a --prune-empty-dirs --ignore-existing --include '*/' ${database_template}/ ${WSO2_SERVER_HOME}/repository/database/
 
 # copy any configuration changes mounted to config_volume
-test -d ${config_volume} && [ "$(ls -A ${config_volume})" ] && cp -RL ${config_volume}/* ${WSO2_SERVER_HOME}/
+if [ -z "$(ls -A ${config_volume})" ]; then
+   echo "Configuration is empty, not copying files."
+else
+   echo "Copying configuration files."
+   cp -RL ${config_volume}/* ${WSO2_SERVER_HOME}/
+fi
+
 # copy any artifact changes mounted to artifact_volume
-test -d ${artifact_volume} && [ "$(ls -A ${artifact_volume})" ] && cp -RL ${artifact_volume}/* ${WSO2_SERVER_HOME}/
+if [ -z "$(ls -A ${artifact_volume})" ]; then
+   echo "Artifacts is empty, not copying files."
+else
+   echo "Copying artifact files."
+   cp -RL ${artifact_volume}/* ${WSO2_SERVER_HOME}/
+fi
+
+cat ${WSO2_SERVER_HOME}/repository/conf/carbon.xml
 
 # start WSO2 Carbon server
 sh ${WSO2_SERVER_HOME}/bin/wso2server.sh "$@"
